044f82303048d0a182ff48291306db2a75f20304
hector <hectoreddbenitez@gmail.com>
2022-10-03 13:04:35 -0300

    feat(req04): implementa função scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,35 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url = selector.css("link[rel=canonical] ::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css("span.author a::text").get().strip()
+    coments = len(selector.css(".comment-list li").getall())
+    if coments is None:
+        coments = 0
+    sumary = "".join(
+        selector.css(".entry-content > p:first-of-type ::text").getall()
+    ).strip()
+    tags = selector.css("a[rel=tag]::text").getall()
+    category = selector.css(".label ::text").get()
+
+    # referencia(.strip()):
+    # https://www.delftstack.com/pt/howto/python/how-to-remove-whitespace-in-a-string/
+    # referencia(first-of-type):
+    # https://developer.mozilla.org/pt-BR/docs/Web/CSS/:first-of-type
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": coments,
+        "summary": sumary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
