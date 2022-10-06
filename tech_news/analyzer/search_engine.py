f7d8f89b018bc1f15b8247c88d6b23b297f2bd7d
hector <hectoreddbenitez@gmail.com>
2022-10-06 18:54:56 -0300

    feat(req07): implementa função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 import tech_news
+from datetime import datetime
 
 database = tech_news.database
 
@@ -9,13 +10,27 @@ def search_by_title(title):
         {"title": {"$regex": title, "$options": "i"}}
     )
     for item in response:
-        lista.append((item["title"], item["url"])) 
+        lista.append((item["title"], item["url"]))
     return lista
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    lista = []
+    try:
+        # referencia strptime
+        # https://parzibyte.me/blog/2020/04/23/validar-fecha-python/
+        # referencia strftime:
+        # https://www.programiz.com/python-programming/datetime/strftime
+        dateOk = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        response = database.search_news(
+            {"timestamp": {"$regex": dateOk, "$options": "i"}}
+        )
+        for item in response:
+            lista.append((item["title"], item["url"]))
+    except ValueError:
+        raise ValueError("Data inválida")
+    return lista
 
 
 # Requisito 8
