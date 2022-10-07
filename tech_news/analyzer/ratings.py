f7fbf1c5f811af7b7a38748734345301e8ebb124
hector <hectoreddbenitez@gmail.com>
2022-10-06 21:00:47 -0300

    feat(req10): implementa função top_5_news_

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,17 @@
-# Requisito 10
+import tech_news
+
+database = tech_news.database
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    list = []
+    response = database.find_news()
+    ordered_response = sorted(
+        response, key=lambda item: item["comments_count"], reverse=True
+    )
+    for item in ordered_response[:5]:
+        list.append((item["title"], item["url"]))
+    return list
 
 
 # Requisito 11
